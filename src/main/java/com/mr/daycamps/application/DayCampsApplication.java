5f1fcfdeb6c88923cc7aeacdb42344eaa3b6746c
mreda <reda.mateusz@gmail.com>
2022-03-03 23:53:13 +0100

    Initial commit

---
+++ src/main/java/com/mr/daycamps/application/DayCampsApplication.java
@@ -0,0 +1,13 @@
+package com.mr.daycamps.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayCampsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayCampsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mr/daycamps/application/DayCampsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.daycamps.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayCampsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
