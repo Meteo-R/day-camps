0571b3704d2c84511b016fe2c6f46ab1aa56e7b2
mreda <reda.mateusz@gmail.com>
2022-03-03 23:53:13 +0100

    Initial commit

--- src/main/java/com/mr/daycamps/application/DayCampsApplication.java
+++ src/main/java/com/mr/daycamps/application/DayCampsApplication.java
@@ -2,8 +2,10 @@ package com.mr.daycamps.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.mr.daycamps")
 public class DayCampsApplication {
 
     public static void main(String[] args) {
